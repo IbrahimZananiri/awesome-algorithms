e0e7fc0aac40f56c5474848ce03fc314f3b6d795
IbrahimZananiri <ibrahim.zananiri@gmail.com>
2022-07-16 01:38:12 +0300

    Create Graph.java

---
+++ graph/Graph.java
@@ -0,0 +1,15 @@
+public interface Graph<Vertex> extends Iterable<Vertex> {
+    // Vertices methods
+    int size();
+    void addVertex(Vertex vertex);
+    void removeVertex(Vertex vertex);
+
+    // Edges Methods
+    boolean hasEdge(Vertex source, Vertex destination);
+    void addEdge(Vertex source, Vertex destination, int weight);
+    void removeEdge(Vertex source, Vertex destination);
+
+    // Edge Weight Methods
+    Integer getEdgeWeight(Vertex source, Vertex destination);
+    void setEgeWeight(Vertex source, Vertex destination, int weight);
+}
